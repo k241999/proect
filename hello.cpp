7c70fa91d03b4599673c031d3dee21d4aff5ee22
k241999 <151376802+k241999@users.noreply.github.com>
2023-11-18 13:59:50 +0000

    argc argv

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc,char** argv) {
+  for (int i = 0; i < argc; i++)
+  {
+    cout << i << " -- ";
+    cout<< argv[i] << endl;
+    
+
+  }
+    cout << "Hello World!"<< endl;
+
     return 0;
 }
